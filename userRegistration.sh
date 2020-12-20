a446a153e8a8054d6cc23960a87bc0afc09cb5cf
Saiseshu Reddy <saiseshureddy.isanaka@gmail.com>
2020-12-20 09:03:38 +0530

    First Name Validation user case

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to email User Registration"
+
+#First Name Validation User Case
+read -p "Enter the first name which starts with cap and has a minimum of 3 characters:" firstName
+firstNamePattern="^[A-Z]{1}[a-zA-Z0-9]{2}([a-zA-Z0-9])*$"
+if [[ $firstName =~ $firstNamePattern ]]
+then
+        echo "Valid first name"
+else
+        echo "Invalid first name"
+fi
